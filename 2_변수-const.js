5401c5c01805c5849d722f6197d7a94f3c56286e
Son Seung Gyu <thstmd0705@naver.com>
2023-11-27 13:45:30 +0900

    js init

---
+++ 2_변수-const.js
@@ -0,0 +1,9 @@
+
+const a = 12;
+// a = 14;  const로 선언된 변수는 상수로 이후에 값 변경 불가능
+
+
+let b = 12;
+b = 999;
+console.log(a);
+console.log(b);
---
+++ 3_기초자료형.js
@@ -0,0 +1,27 @@
+//String(문자열)
+const myName = "둘리"; //큰 따옴표
+const email = 'user@naver.com'; //작은 따옴표
+//백틱. ${} 기호를 사용해서 문자열에 변수를 포함시킴
+const hello = `Hello ${myName}`;
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+
+//Number(숫자)
+const number = 123; //정수
+const opacity = 0.57; //실수
+console.log(number);
+console.log(opacity);
+
+//Boolean(논리형)
+let checked = true;
+let isShow = false;
+console.log(checked);
+console.log(isShow);
+
+let undef;// 값을 대입하지 않으면 undefined가 자동으로 할당
+console.log(undef);
+
+let name = null;//개발자가 직접 대입하면 null
+console.log(name);
